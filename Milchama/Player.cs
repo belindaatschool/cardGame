e7ae8f9677113a87cf5d1aae2548e0d101675097
Belinda KA <107852570+belindaatschool@users.noreply.github.com>
2025-06-12 15:52:15 +0300

    Update Player.cs

--- Milchama/Player.cs
+++ Milchama/Player.cs
@@ -2,14 +2,15 @@ using System;
 
 public class Player
 {
+    private string name;
     private char[] cardTypes = { '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A' };
     private char[] deck;
     private int currentIndex = 0;
     private int points = 0;
 
-    public Player(Random random)
+    public Player(Random random, string name)
     {
-        deck = new char[26];
+        deck = new char[10];
 
         for (int i = 0; i < deck.Length; i++)
         {
@@ -18,10 +19,33 @@ public class Player
 
         this.points = 0;
         this.currentIndex = deck.Length - 1;
+        this.name = name;
     }
+
+    public int GetCurrentIndex() { 
+        return currentIndex; 
+    }
+
 
     public char GetTopCard()
     {
+        if (currentIndex < 0)
+            return ' ';
         return deck[currentIndex--];
+    }
+
+    public void AddPoints(int points)
+    {
+        this.points += points;
+    }
+
+    public int GetPoint()
+    {
+        return points;
+    }
+
+    public string GetName()
+    {
+        return name;
     }
 }
